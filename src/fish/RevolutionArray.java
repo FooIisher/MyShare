e7f6995703c139144ea2e1e132c9027fd5fd62b8
fish <yujinlin1993@gmail.com>
2017-03-14 09:54:48 +0800

    update

--- src/fish/RevolutionArray.java
+++ src/fish/RevolutionArray.java
@@ -1,4 +1,7 @@
 package fish;
+
+import java.util.ArrayList;
+
 /**
  * 顺时针打印一个数组
  * @author fish
@@ -15,6 +18,9 @@ public class RevolutionArray {
 			}
 			System.out.println("");
 		}
+		System.out.println("-----------------------------");
+		ArrayList<Integer> list = printMatrix(result);
+		System.out.print(list);
 	}
 
 	private static int[][] getLostNum(int num) {
@@ -39,6 +45,54 @@ public class RevolutionArray {
 		}
 		return result;
 	}
+	
+
+	/**
+	 * 输入一个矩阵，按照从外向里以顺时针的顺序依次打印出每一个数字
+	 * @param matrix
+	 * @return
+	 */
+	private static ArrayList<Integer> printMatrix(int [][] matrix) {
+	        ArrayList<Integer> result = new ArrayList<Integer>();
+	      	int weith = matrix.length;
+	        int length = matrix[0].length;
+	        int x=0,y=0;
+	        int i=1;
+	       
+	        result.add(matrix[0][0]);
+	        matrix[0][0]=-1;
+	        
+	        while(i<weith*length){
+	            while(y+1<length&&matrix[x][y+1]!=-1){
+	            	++y;
+	            	result.add(matrix[x][y]);
+	                matrix[x][y]=-1;
+	                i++;
+	            }
+	            
+	             while(x+1<weith&&matrix[x+1][y]!=-1){
+	            	 ++x;
+	                result.add(matrix[x][y]);
+	                matrix[x][y]=-1;
+	                i++;
+	            }
+	            
+	            while(y-1>=0&&matrix[x][y-1]!=-1){
+	            	--y;
+	                result.add(matrix[x][y]);
+	                matrix[x][y]=-1;
+	            	i++;
+	            }
+	            
+	            while(x-1>=0&&matrix[x-1][y]!=-1){
+	            	--x;
+	                result.add(matrix[x][y]);
+	                matrix[x][y]=-1;
+	                i++;
+	            }
+	        }
+	        return result;
+	    }
 
 	
 
