08d92313f938f18a38697843b795b5b6df28c7d1
fish <yujinlin1993@gmail.com>
2017-03-16 11:06:56 +0800

    update

--- src/fish/Test.java
+++ src/fish/Test.java
@@ -1,38 +1,40 @@
 package fish;
 
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.TreeMap;
 
-public class Test {
-
-	public static void main(String[] args) {
-		
-		HashMap<Integer,Object> map = new HashMap<Integer,Object>();
-		map.put(1, new Demo(1,12));
-		map.put(2, new Demo(2,22));
-		map.put(3, new Demo(1,11));
-		map.put(4, new Demo(4,19));
-		
-
-
-		ThreadDemo td1  = new  ThreadDemo(map);
-		ThreadDemo td2  = new  ThreadDemo(map);
-		ThreadDemo td3  = new  ThreadDemo(map,2);
-//		ThreadDemo td4  = new  ThreadDemo(map);
-//		ThreadDemo td5  = new  ThreadDemo(map);
-		
-		td1.start();
-		td2.start();
-		td3.start();
-		
-		System.out.println(Integer.MAX_VALUE);
-
-	}
-
-	
-		
-
-		
-	
-}
+public class Test {  
+    public static void main(String [] args){  
+        System.out.println(new B().getValue());  
+    }  
+    static class A{  
+        protected int value;  
+        public A(int v) {  
+            setValue(v);  
+        }  
+        public void setValue(int value){  
+            this.value = value;  
+        }  
+        public int getValue(){  
+            try{  
+                value++;  
+                return value;  
+            } catch(Exception e){  
+                System.out.println(e.toString());  
+            } finally {  
+                this.setValue(value);  
+                System.out.println(value);  
+            }  
+            return value;  
+        }  
+    }  
+    
+    
+    static class B extends A{  
+        public B() {  
+            super(5);  
+            setValue(getValue() - 3);  
+        }  
+        public void setValue(int value){  
+            super.setValue(2 * value);  
+        }  
+    }  
+}  
