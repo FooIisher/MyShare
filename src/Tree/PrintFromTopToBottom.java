53c7dc0adbd128ea932054bf105d46cf08b98c05
fish <yujinlin1993@gmail.com>
2017-03-20 13:57:44 +0800

    update

--- src/Tree/PrintFromTopToBottom.java
+++ src/Tree/PrintFromTopToBottom.java
@@ -31,4 +31,60 @@ public class PrintFromTopToBottom {
        
     }
 	
+	 public ArrayList<ArrayList<Integer>> Print(TreeNode pRoot) {
+		  ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+		  ArrayList<Integer> records = new ArrayList<Integer>();
+		  Queue<TreeNode> queue1 = new LinkedList<TreeNode>();
+		  Queue<TreeNode> queue2 = new LinkedList<TreeNode>();
+		  if(pRoot!=null){
+			  queue1.add(pRoot);
+		  }
+		  
+		  while(!queue1.isEmpty()||!queue2.isEmpty()){
+			  while(!queue1.isEmpty()){
+				  TreeNode temp = queue1.poll();
+				  if(temp.left!=null){
+					  queue2.offer(temp.left);
+				  }
+				  if(temp.right!=null){
+					  queue2.offer(temp.right);
+				  }
+				  records.add(temp.val);
+			  }
+			  if(records.size()!=0){
+				  result.add(records);
+			  }
+			  records =  new ArrayList<Integer>(); 
+			  
+			  
+			  while(!queue2.isEmpty()){
+				  TreeNode temp = queue2.poll();
+				  if(temp.left!=null){
+					  queue1.offer(temp.left);
+				  }
+				  if(temp.right!=null){
+					  queue1.offer(temp.right);
+				  }
+				  records.add(temp.val);
+			  }
+			  if(records.size()!=0){
+				  result.add(records);
+			  }
+			  records =  new ArrayList<Integer>();   
+		  }
+		return result;
+	 }
+	 
+	 public static void main(String[] args){
+			//{8,6,10,5,7,9,11},1
+			TreeNode root = new TreeNode(1);
+			root.left = new TreeNode(2);
+			root.left.left = new TreeNode(4);
+			root.left.right = new TreeNode(5);
+			root.right = new TreeNode(3);
+			root.right.left = new TreeNode(6);
+			root.right.right = new TreeNode(7);
+			PrintFromTopToBottom print = new PrintFromTopToBottom();
+			System.out.print(print.Print(root));
+		}
 }
