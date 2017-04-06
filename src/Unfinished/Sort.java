7cf268436080713b28018c6d6f7c9160680ec723
unknown <yujinlin1993@gmail.com>
2017-04-06 17:59:21 +0800

    update

--- src/Unfinished/Sort.java
+++ src/Unfinished/Sort.java
@@ -2,6 +2,58 @@ package Unfinished;
 
 public class Sort {
 	
+	/**
+	 * 将两个（或两个以上）有序表合并成一个新的有序表 即把待排序序列分为若干个子序列，每个子序列是有序的。
+	 * 然后再把有序子序列合并为整体有序序列 
+     * 时间复杂度为O(nlogn) 
+	 * @param nums
+	 * @param start
+	 * @param end
+	 */
+	public static void mergeSort(int[] nums,int start,int end){
+		if(start<end){
+			int mid = (start+end)/2;
+			mergeSort(nums,start,mid);
+			mergeSort(nums,mid+1,end);
+			mergeSortHandler(nums, start,mid, end);
+		}
+		return;
+	}
+	
+	
+	private static void mergeSortHandler(int[] nums, int start, int mid, int end) {
+		int[] temp = new int[end-start+1];
+		int i =start;
+		int j =mid+1;
+		int k =0;
+		while(i<=mid&&j<=end){
+			if(nums[i]<nums[j]){
+				temp[k++] = nums[i++];
+			}else{
+				temp[k++] = nums[j++];
+			}
+		}
+		
+		while(i<=mid){
+			temp[k++]=nums[i++];
+		}
+		
+		while(j<=end){
+			temp[k++]=nums[j++];
+		}
+		
+		for(int m=0;m<temp.length;m++){
+			nums[start+m] = temp[m];
+		}
+	}
+
+
+	/**
+	 * 快速排序
+	 * @param nums
+	 * @param start
+	 * @param end
+	 */
 	public static void fastSort(int[] nums,int start,int end){
 		if(start >= end)
 	    {
@@ -30,7 +82,8 @@ public class Sort {
 
 	public static void main(String[] args) {
 		int[] nums = {1,2,4,6,7,3,6,8,10};
-		fastSort(nums, 0, nums.length-1);
+//		fastSort(nums, 0, nums.length-1);
+		mergeSort(nums, 0, nums.length-1);
 		for(int num:nums){
 			System.out.print(num+" ");
 		}
