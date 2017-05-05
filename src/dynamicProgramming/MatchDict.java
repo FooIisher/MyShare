5960c484c12690564ed326b65d8766622009ff9f
unknown <yujinlin1993@gmail.com>
2017-05-05 16:36:18 +0800

    update

--- src/dynamicProgramming/MatchDict.java
+++ src/dynamicProgramming/MatchDict.java
@@ -13,65 +13,103 @@ import java.util.Set;
  * dict =["cat", "cats", "and", "sand", "dog"].
  * A solution is["cats and dog", "cat sand dog"].
  * @author fish
- *	思路：利用数组 record[i][j] 来标记s.sub(i,j)是否存在字典中中，根据记录值进行递归（时间复杂度过高！leetcode不通过）
+ *	思路：利用数组 record[i][j] 来标记s.sub(i,j)是否存在字典中中，根据记录值进行递归（空间复杂度过高！leetcode不通过）
  */
 public class MatchDict {
 
-	 public static ArrayList<String> wordBreak(String s, Set<String> dict) {
+	
+	/**
+	 * 利用矩阵来解决 超时遍历
+	 * 
+	 * @param s
+	 * @param dict
+	 * @return
+	 */
+	public static ArrayList<String> wordBreak2(String s, Set<String> dict) {
 		ArrayList<String> result = new  ArrayList<String>();
+		ArrayList<String> one = new ArrayList<String>();
 		int[][] records = new int[s.length()][s.length()+1];
 		for(int i=0;i<s.length();i++){
-			for(int j=i+1;j<s.length()+1;j++){
-				if(j==s.length()){
-					if(dict.contains(s.substring(i))){
-						records[i][j]=1;
-					}
-				}else{
-					if(dict.contains(s.substring(i, j))){
-						records[i][j]=1;
-					}
+			for(int j=i+1;j<=s.length();j++){
+				if(dict.contains(s.substring(i, j))){
+					records[i][j] = 1;
 				}
 			}
-		
 		}
-		StringBuilder sb = new StringBuilder();
-		find(result,0,sb,records,s);
-		
+		find2(result,one,0,records,s);
 		return result;
-	 }
+	}
+	
+	
 	
 
-	private static void find(ArrayList<String> result, int row, StringBuilder sb, int[][] records, String s) {
-		for(int i=0;i<=s.length();i++){
-			if(records[row][i]==1){
-				if(row==0){
-					sb = new StringBuilder();	
+	 private static void find2(ArrayList<String> list, ArrayList<String> one,
+			int start, int[][] records, String s) {
+		 if(start==s.length()){
+				StringBuilder sb = new StringBuilder();
+				for(String word:one){
+					sb.append(word+" ");
 				}
-				
-				if(i==s.length()){
-					sb.append(s.substring(row)+" ");
-					result.add(sb.toString().trim());
-				}else{
-					sb.append(s.substring(row,i)+" ");
-					find(result, i, sb, records, s);
+				list.add(sb.toString().trim());
+			}else{
+				for(int i=start;i<=s.length();i++){
+					if(records[start][i]==1){
+						one.add(s.substring(start, i));
+						find2(list, one, i, records, s);
+						one.remove(one.size()-1);
+					}
 				}
-					
+			}
+		
+	}
+
+
+
+
+	public static ArrayList<String> wordBreak(String s, Set<String> dict) {
+			ArrayList<String> list = new  ArrayList<String>();
+			ArrayList<String> one = new ArrayList<String>(); 
+			find(0,one,list,s,dict);
+			return list;
+		 }
+		
+	
+
+	private static void find(int start, ArrayList<String> one,
+			ArrayList<String> list, String s, Set<String> dict) {
+		if(start==s.length()){
+			StringBuilder sb = new StringBuilder();
+			for(String word:one){
+				sb.append(word+" ");
+			}
+			list.add(sb.toString().trim());
+		}else{
+			for(int i=start;i<=s.length();i++){
+				if(dict.contains(s.substring(start, i))){
+					one.add(s.substring(start, i));
+					find(i, one, list, s, dict);
+					one.remove(one.size()-1);
+				}
 			}
 		}
-		
 		
 	}
 
 
 	public static void main(String[] args) {
-		String s ="a";
+		String s ="catsanddog";
 		Set<String> dict = new HashSet<String>();
-		String[] ins = {"a"};
+		String[] ins = {"cat", "cats", "and", "sand","dog"};
 		for(String in:ins){
 			dict.add(in);
 		}
 		
-		System.out.println(wordBreak(s, dict));
+		System.out.println(wordBreak2(s, dict));
+//		System.out.println(wordBreak(s, dict));
+	
 	}
+
+
+
 
 }
